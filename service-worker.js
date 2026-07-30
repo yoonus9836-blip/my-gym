83eb2a379fef052770180d3e3065ae885b6777c0
yoonus9836-blip <yoonus9836@gmail.com>
2026-07-30 17:10:15 +0400

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,1 @@
+const C='yoonus-gym-v3',A=['./','./index.html','./styles.css','./app.js','./manifest.json','./assets/icon-192.png','./assets/icon-512.png'];self.addEventListener('install',e=>e.waitUntil(caches.open(C).then(c=>c.addAll(A))));self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
